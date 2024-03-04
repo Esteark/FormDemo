23feeacd2de8115e2e08a6c985f8a3f0a3839a7b
esteark <eardila1409@gmail.com>
2024-03-04 15:00:11 -0500

    Add function show password

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./stylesForm.scss";
 import { useForm } from "react-hook-form";
 import useValidate from "../../hooks/useValidate.js";
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from "react-hot-toast";
 import { MdError } from "react-icons/md";
-import Swal, { swal } from "sweetalert2/dist/sweetalert2.js";
+import Swal from "sweetalert2/dist/sweetalert2.js";
 import "sweetalert2/src/sweetalert2.scss";
 import { setSesionUser } from "../../services/infoLocal.js";
+import { IoEyeOffOutline } from "react-icons/io5";
+import { FaRegEye } from "react-icons/fa6";
 
 const Form = () => {
   const navigate = useNavigate();
+  const [show, setShow] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  //Iniciamos el hook personalido
+  //Iniciamos el hook personalizado que me ayuda a validar mediante expresiones regulares las especificaciones requeridas
+  //Tales como , el nombre no debe estar vacio , el email debe de ser uno válido
+  //La contraseña debe contener almenos una mayuscula , una minuscula , un carácter especial y 8 carácteres
   const validacion = useValidate();
 
   //   la siguiente función valida la información del formulario
@@ -32,11 +37,15 @@ const Form = () => {
         showConfirmButton: false,
         timer: 1500,
       });
-      //enviamos la información al servidor orealizamos otras acciones en este caso almacenamos la información en el local storage
+      //enviamos la información al servidor o realizamos otras acciones en este caso almacenamos la información en el local storage
       setSesionUser(data);
       //navegamos ala otra ventana
       navigate("/success");
     }, 1000);
+  };
+
+  const showHidePassWord = () => {
+    setShow(!show);
   };
 
   return (
@@ -44,7 +53,7 @@ const Form = () => {
       <Toaster />
       <form className="form" onSubmit={handleSubmit(onSubmitForm)}>
         <h2 className="titleForm">
-          Start Your Journey <br /> with a 7-day free trial{" "}
+          Start Your Journey <br /> with a 7-day free trial
         </h2>
         <label htmlFor="" className="labelForm">
           First name*
@@ -75,7 +84,7 @@ const Form = () => {
         <label htmlFor="" className="labelForm">
           Password*
           <input
-            type="password"
+            type={!show ? "password" : "text"}
             placeholder="Create a password"
             className="inputForm"
             {...register("pass", {
@@ -87,6 +96,21 @@ const Form = () => {
               },
             })}
           />
+          {!show ? (
+            <FaRegEye
+              className="iconShow"
+              onClick={() => {
+                showHidePassWord();
+              }}
+            />
+          ) : (
+            <IoEyeOffOutline
+              className="iconShow"
+              onClick={() => {
+                showHidePassWord();
+              }}
+            />
+          )}
         </label>
 
         <section className="secErrors">
